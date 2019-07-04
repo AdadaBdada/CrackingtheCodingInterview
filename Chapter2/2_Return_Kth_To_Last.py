abc5a96ccae2f5a766160206ce33e67b1c0be1f9
Shuo <sjin6@stevens.edu>
2019-07-04 14:49:01 -0400

    Return Kth to Last

---
+++ Chapter2/2_Return_Kth_To_Last.py
@@ -0,0 +1,44 @@
+class ListNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def kth_to_last(head, k):
+
+    right = head
+    left = head
+
+    for i in range(k):
+
+        if not right.next:
+            raise LookupError('Error: n is larger than the linked list.')
+
+        right = right.next
+
+    while right:
+
+        right = right.next
+        left = left.next
+
+    return left
+
+
+if __name__ == "__main__":
+    # ListNode
+    a = ListNode(1)
+    b = ListNode(1)
+    c = ListNode(3)
+    d = ListNode(2)
+    e = ListNode(2)
+
+    # link ListNode
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+
+    left = kth_to_last(a, 2)
+    print(left.val)
+    print(left.next.val)
