397df0920266a5e05b00be3181ce395d8625ab5e
Shuo <sjin6@stevens.edu>
2019-07-29 15:40:41 -0400

    update inorder successor

--- Chapter4/6_Inorder_Successor.py
+++ Chapter4/6_Inorder_Successor.py
@@ -1,23 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
-class Solution:
-    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+def inorderSuccessor(root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
 
-        ans = None
-        while root:
-            if p.val < root.val:
-                ans = root
-                root = root.left
-            else:
-                root = root.right
+    ans = None
+    while root:
+        if p.val < root.val:
+            ans = root
+            root = root.left
+        else:
+            root = root.right
 
-        return ans
+    return ans
 
 
 if __name__ == "__main__":
+
+    root = TreeNode(2)
+    root.left, root.right = TreeNode(1), TreeNode(3)
+    res = inorderSuccessor(root, p=TreeNode(1))
+    print(res.val)
